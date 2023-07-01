6a60cff84e2e69c0f709a898d71e42608ad25f05
Carlos Vargas Arce <woxry@Carloss-MacBook-Pro.local>
2023-07-01 13:25:32 -0600

    Base Project

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const ContactPage = () => {
+  return (
+    <main>
+      <h1>Contact Page</h1>
+    </main>
+  )
+}
+
+export default ContactPage
+
+export const Head = () => <title>Contact Page</title>
